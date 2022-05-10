fe1aeba051414195b2e4bfef628793717d54a297
Arce Juan Eduardo <arce.juan.lua@gmail.com>
2022-05-10 17:56:53 -0300

    Modified Persistence

---
+++ Persistence/Configurations/ClientConfig.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations
+{
+    public class ClientConfig : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.ToTable("Clients");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.LastName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(p => p.DateBirth)
+                .IsRequired();
+
+            builder.Property(p => p.PhoneNumber)
+                .HasMaxLength(10)
+                .IsRequired();
+
+            builder.Property(p => p.Email)
+                .HasMaxLength(30);
+
+            builder.Property(p => p.Address)
+                .HasMaxLength(30);
+
+            builder.Property(p => p.Age);
+
+            builder.Property(p => p.CreateBy)
+                .HasMaxLength(10);
+
+            builder.Property(p => p.LastModifiedBy)
+                .HasMaxLength(10);
+        }
+    }
+}
---
+++ Persistence/Contents/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces;
+using Domain.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Persistence.Contents
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IDateTimeService _dateTime;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IDateTimeService dateTime) : base(options)
+        {
+            _dateTime = dateTime;
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking; // controla que entityFramework va a mantener toda la info sobre una intancia de cambios de una entidad
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableBaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreationDate = _dateTime.NowUtc;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = _dateTime.NowUtc;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // 
+        }
+    }
+}

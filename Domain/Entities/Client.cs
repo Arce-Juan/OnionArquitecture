de3e65d639c3ac29c2abbe128540e8680c400e0e
Arce Juan Eduardo <arce.juan.lua@gmail.com>
2022-05-10 11:33:23 -0300

    Added packets to all projects. Modified domain

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using System;
+
+namespace Domain.Entities
+{
+    public class Client : AuditableBaseEntity
+    {
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                this._age = new DateTime(DateTime.Now.Subtract(this.DateBirth).Ticks).Year - 1;
+
+                return this._age;
+            }
+        }
+    }
+}
